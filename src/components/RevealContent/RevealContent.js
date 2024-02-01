858c178542fe0ae006e2ed94be43a3a38f51c639
Usman Butt <usman.butt@outlook.com>
2024-02-01 14:45:26 -0500

    Add reveal functionality to RevealContent component

--- src/components/RevealContent/RevealContent.js
+++ src/components/RevealContent/RevealContent.js
@@ -1,7 +1,24 @@
+'use client';
 import React from 'react';
 
-function RevealContent() {
-  return <div></div>;
+function RevealContent({ children }) {
+  const [isSnippetShown, setSnippetShown] = React.useState(false);
+
+  function handleReveal() {
+    setSnippetShown(true);
+  }
+
+  return (
+    <>
+      {isSnippetShown ? (
+        children
+      ) : (
+        <div className={'reveal'}>
+          <button onClick={handleReveal}>Reveal Content</button>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default RevealContent;
